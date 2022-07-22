0f95ed5f012b5e8b133ddcf4e6d8fa20dbb33885
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-07-22 19:16:51 +0900

    layout ui

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,15 @@
+import './style.css';
+import Header from "../header/Header";
+import Form from "../form/Form";
+
+
+function Layout() {
+    return (
+      <div>
+      <Header></Header>
+      <Form></Form>
+      </div>
+    );
+  }
+  
+  export default Layout;
