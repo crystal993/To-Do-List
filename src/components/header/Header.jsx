8bc50211345efa3d5a09d530156e88749c224a55
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-07-22 19:16:31 +0900

    header ui

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+function Header() {
+    return (
+      <div class="header-container">
+        <p>My Todo List</p>
+      </div>
+    );
+  }
+  
+  export default Header;
