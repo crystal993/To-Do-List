a20b2b4915041f00cf5d3f93f9dabfd65837c2f3
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-07-23 23:43:58 +0900

    prettier code

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,52 +1,60 @@
-import React, {useState} from "react";
-import './style.css';
+import React, { useState } from "react";
+import "./style.css";
 import Todo from "../todo/Todo";
 
 function List({ setTodoList, todoList }) {
   const onDeleteHanlder = (todoId) => {
-    const newToDos = todoList.filter((todo)=>todo.id !== todoId);
+    const newToDos = todoList.filter((todo) => todo.id !== todoId);
     setTodoList(newToDos);
-  }
+  };
 
   const onToggleHandler = (todoId) => {
-    const newToDos = todoList.map(todo => todo.id === todoId ? {...todo, isDone: !todo.isDone} : todo)
-    setTodoList(newToDos);
-  }
-  
-    return (
-      <div className='list-container'>
-        <h3>Working.. 🔥</h3>
-        <div className='to-do-container'>
-            {todoList.map((todo) => {
-             return <div className="item"> 
-             {todo.isDone === false && 
-              <Todo
-                todo={todo}
-                key={todo.id}
-                onDeleteHanlder={onDeleteHanlder}
-                onToggleHandler={onToggleHandler}
-              />} </div>
-            })}
-        </div>
-        <h3>Done..!🎉</h3>
-        {/* <Todo todo={todo} key={todo.id}></Todo> */}
-        <div className='to-do-container'>
-            { 
-              todoList.map((todo)=>{
-                // eslint-disable-next-line no-lone-blocks
-                return <div className="item">
-                {todo.isDone === true && 
-                      <Todo
-                        todo={todo}
-                        key={todo.id}
-                        onDeleteHanlder={onDeleteHanlder}
-                        onToggleHandler={onToggleHandler}
-                      />
-                  }</div>
-              })}
-        </div>
-      </div>
+    const newToDos = todoList.map((todo) =>
+      todo.id === todoId ? { ...todo, isDone: !todo.isDone } : todo
     );
-  }
-  
-  export default List;
+    setTodoList(newToDos);
+  };
+
+  return (
+    <div className="list-container">
+      <h3>Working.. 🔥</h3>
+      <div className="to-do-container">
+        {todoList.map((todo) => {
+          return (
+            <div className="item">
+              {todo.isDone === false && (
+                <Todo
+                  todo={todo}
+                  key={todo.id}
+                  onDeleteHanlder={onDeleteHanlder}
+                  onToggleHandler={onToggleHandler}
+                />
+              )}{" "}
+            </div>
+          );
+        })}
+      </div>
+      <h3>Done..!🎉</h3>
+      {/* <Todo todo={todo} key={todo.id}></Todo> */}
+      <div className="to-do-container">
+        {todoList.map((todo) => {
+          // eslint-disable-next-line no-lone-blocks
+          return (
+            <div className="item">
+              {todo.isDone === true && (
+                <Todo
+                  todo={todo}
+                  key={todo.id}
+                  onDeleteHanlder={onDeleteHanlder}
+                  onToggleHandler={onToggleHandler}
+                />
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default List;
